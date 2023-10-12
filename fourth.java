97367ebce7dc9c648525606c0b48b617ee34fb15
David Osborn <davidosborn87@protonmail.com>
2023-10-12 13:03:37 +0100

    first alteration

---
+++ fourth.java
@@ -0,0 +1,11 @@
+package fourth;
+
+public class fourth {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Now fuck off");
+    }
+}
+
+//this is to test git commit
+
+
